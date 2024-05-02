d52c78674d7874bdb2cf6949965906343554ecbd
DannyMichaels <itzdanielmichael@gmail.com>
2024-05-02 15:55:51 -0400

    update

--- src/app/api/marketstack/eod-latest/route.js
+++ src/app/api/marketstack/eod-latest/route.js
@@ -2,16 +2,19 @@ import { NextResponse } from 'next/server';
 import { getEodLatest, getTickers } from '../../../../services/marketstackApi';
 
 export async function GET(request) {
-  // const url = new URL(request.url);
+  try {
+    // const url = new URL(request.url);
 
-  const tickers = await getTickers();
-  console.log({ tickers });
-  const symbols = tickers.data.map((ticker) => ticker.symbol).join(',');
+    const tickers = await getTickers();
+    const symbols = tickers.data.map((ticker) => ticker.symbol).join(',');
 
-  // const searchParams = new URLSearchParams(url.searchParams);
-  // const symbols = searchParams.get('symbols');
+    // const searchParams = new URLSearchParams(url.searchParams);
+    // const symbols = searchParams.get('symbols');
 
-  const data = await getEodLatest(symbols);
+    const data = await getEodLatest(symbols);
 
-  return NextResponse.json(data, { status: 200 });
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
