be129128e94f88de6ef0e556c18b200886c19937
DannyMichaels <itzdanielmichael@gmail.com>
2024-05-02 16:54:13 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -21,8 +21,8 @@ const nextConfig = {
       },
     ];
   },
-  output: 'export', // Outputs a Single-Page Application (SPA).
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  // output: 'export', // Outputs a Single-Page Application (SPA).
+  // distDir: './dist', // Changes the build output directory to `./dist/`.
   // basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
   // output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
   output: 'standalone',
